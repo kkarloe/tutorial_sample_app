dbc7494a0c5015bea349a28e10fb52ffbb626bcc
Karol Kaczmarek <karol.m.kaczmarek@gmail.com>
2014-04-02 18:10:36 -0700

    Adding ajax for buttons and improving feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ def User.hash(token)
 end
 
 def feed
-  Micropost.where("user_id = ?", id)
+  Micropost.from_users_followed_by(self)
 end
   
 def following?(other_user)
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -22,37 +22,48 @@ describe "Authentication" do
 	        before { patch user_path(wrong_user) }
 	        specify { expect(response).to redirect_to(root_url) }
 	      end
-	   end
+	     end # end as wrong user
     
-    describe "in the Microposts controller" do
-      describe "submitting to the create action" do
-        before { post microposts_path }
-        specify {expect(response).to redirect_to(signin_path)}
-      end
-      
-      describe "submitting to the destroy action" do
-        before { delete micropost_path(FactoryGirl.create(:micropost))}
-        specify {expect(response).to redirect_to(signin_path)}
-      end
-    end
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify {expect(response).to redirect_to(signin_path)}
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost))}
+          specify {expect(response).to redirect_to(signin_path)}
+        end
+      end # end in the microsoft controller
     
-	  describe "in the Users controller" do
-
-	    describe "visiting the edit page" do
-	      before { visit edit_user_path(user) }
-	      it { should have_title('Sign in') }
-	    end
-
-	    describe "submitting to the update action" do
-	      before { patch user_path(user) }
-	      specify { expect(response).to redirect_to(signin_path) }
-	    end
-
-	    describe "visiting the user index" do
-          before { visit users_path }
+  	  describe "in the Users controller" do
+  
+  	    describe "visiting the edit page" do
+  	      before { visit edit_user_path(user) }
+  	      it { should have_title('Sign in') }
+  	    end
+  
+  	    describe "submitting to the update action" do
+  	      before { patch user_path(user) }
+  	      specify { expect(response).to redirect_to(signin_path) }
+  	    end
+  
+  	    describe "visiting the user index" do
+            before { visit users_path }
+            it { should have_title('Sign in') }
+        end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
           it { should have_title('Sign in') }
         end
-	  end # end for "in the Users controller"
+        
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end  
+          
+  	  end # end for "in the Users controller"
 
 	  	describe "as non-admin user" do
 	      let(:user) { FactoryGirl.create(:user) }
@@ -96,6 +107,18 @@ describe "Authentication" do
 	      		end
 	  		end
       end # end "when attempting to visit protected page"
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end # end "for not-signed in users"
   end # end "authorization"
 
