370d3c0ba60fa8a50fb07f36a29ebe28ba806615
Karol Kaczmarek <karol.m.kaczmarek@gmail.com>
2014-04-05 10:20:33 -0700

    Chapter 11 ex2 using respond_with instead of respond_to

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,21 @@
 class RelationshipsController < ApplicationController
   before_action :signed_in_user
   
+  respond_to :html, :js
+  
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js # automatically calls create.js.erb
-    end
+    respond_with @user
   end 
   
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
+    # using respond_to (optionally can be replaced by respond_with as above)
     respond_to do |format|
       format.html { redirect_to @user }
-      format.js
+      format.js # calls destroy.js.erb
     end
   end
 end
