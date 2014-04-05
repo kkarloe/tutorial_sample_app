8028bd8c9bea63f504c766356ede4746e366a06d
Karol Kaczmarek <karol.m.kaczmarek@gmail.com>
2014-04-05 11:18:55 -0700

    Chapter 11 ex 4 refactoring partial for user_info

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,12 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost = current_user.microposts.build if signed_in?
       @feed_items = current_user.feed.paginate(page: params[:page])
+      @user ||= current_user
+      # Other, longer option:
+      #if (@user == nil )
+      #  @user = current_user
+      #end
+
     end
   end
 
